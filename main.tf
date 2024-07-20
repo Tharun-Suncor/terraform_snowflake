8e2586495b6267df29bc9673d0dbc66ac5340465
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-20 17:14:08 +0530

    new

--- main.tf
+++ main.tf
@@ -4,14 +4,24 @@ terraform {
       source  = "Snowflake-Labs/snowflake"
       version = "0.63.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.state_file_name
   }
 }
 
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
+provider "azurerm" {
+  features {}
 }
+
 provider "snowflake" {
   account     = var.snowflake_account
   username    = var.snowflake_username
@@ -23,7 +33,7 @@ provider "snowflake" {
 module "snowflake_warehouse" {
   source = "./modules/snowflake_warehouse"
 
-  name                 = "my_warehouse33"
+  name                 = "my_warehouse"
   comment              = "This is my warehouse"
   size                 = "X-SMALL"
   auto_suspend         = 60
--- variables.tf
+++ variables.tf
@@ -18,3 +18,26 @@ variable "snowflake_password" {
   type        = string
   sensitive   = true
 }
+
+# Azure Backend Variables
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Name of the container within the Azure Storage Account"
+  type        = string
+  default     = "terraform-state" # Default value can be overridden
+}
+
+variable "state_file_name" {
+  description = "Name of the Terraform state file"
+  type        = string
+  default     = "terraform.tfstate" # Default value can be overridden
+}
