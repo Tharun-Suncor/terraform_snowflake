1bc5fc1e7e2e1ef8d137eaa47a105579244743c1
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-21 21:30:50 +0530

    new

--- variables.tf
+++ variables.tf
@@ -18,19 +18,3 @@ variable "snowflake_password" {
   type        = string
   sensitive   = true
 }
-
-variable "backend_resource_group_name" {
-  description = "Name of the Azure Resource Group for Terraform state"
-}
-
-variable "backend_storage_account_name" {
-  description = "Name of the Azure Storage Account for Terraform state"
-}
-
-variable "backend_container_name" {
-  description = "Name of the Azure Blob container for Terraform state"
-}
-
-variable "backend_key" {
-  description = "Key for the Terraform state file"
-}
