27a08dbb8c902856bb5fbb9b1275113c1628f490
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-22 21:40:38 +0530

    new

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "snowflake_warehouse" {
 
   name                                  = "my_warehouse"
   comment                               = "This is my warehouse"
-  size                                  = "XSMALL"
+  size                                  = "Medium"
   auto_suspend                          = 60
   auto_resume                           = true
   initially_suspended                   = true
