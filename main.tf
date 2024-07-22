10c6a4fc3adadd71522b0b283089a142da02adc0
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-22 12:26:22 +0530

    new

--- main.tf
+++ main.tf
@@ -10,12 +10,13 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg_sf_terraform_backend"
+    storage_account_name = "saterraformbackend22"
+    container_name       = "terrafornstate"
+    key                  = "terraform.tfstate"
+  }
 }
-
-
-
-
 
 provider "azurerm" {
   features {}
