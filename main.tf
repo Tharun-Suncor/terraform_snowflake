2328c30d9e578ebe18539066bb7aab1d623b7966
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-22 16:16:09 +0530

    new

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.61"
+      version = "~> 0.63"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
