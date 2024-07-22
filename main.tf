d162cee1d05588cd666350fbd34fd046915e90e7
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-22 15:54:18 +0530

    new

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "azurerm" {
 
 provider "snowflake" {
   account     = var.snowflake_account
-  username    = var.snowflake_username
+  user        = var.snowflake_username
   password    = var.snowflake_password
   role        = var.snowflake_role
 }
