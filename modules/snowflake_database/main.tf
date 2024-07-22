676926209db2e85a36db960815be353388be56c6
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-07-22 14:49:28 +0530

    new

--- modules/snowflake_database/main.tf
+++ modules/snowflake_database/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.63.0"
+    }
+  }
+}
+
 resource "snowflake_database" "database" {
   name    = var.name
   comment = var.comment
