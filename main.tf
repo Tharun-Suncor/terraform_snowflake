5b6d7e918bc064109222d0ff8e5f5df24d9cfa6e
vaisakh-suncor <151065363+vaisakh-suncor@users.noreply.github.com>
2024-07-29 20:22:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ module "snowflake_schema" {
   is_managed = false
   is_transient = false
 }
-resource "snowsql_exec" "set_default_warehouse" {
+/*resource "snowsql_exec" "set_default_warehouse" {
 create {
     statements           = <<-EOT
     alter user tharunsnow set default_warehouse = 'my_warehouse';
@@ -121,7 +121,7 @@ create {
 delete {
     statements = "select 1;"
   }
-}
+} */
 
 /*resource "snowsql_exec" "db" {
 create {
