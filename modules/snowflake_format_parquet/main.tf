51b21c3311537e5646f876f95e4a2e73b6e78e1d
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 16:33:30 +0530

    Update main.tf

--- modules/snowflake_format_parquet/main.tf
+++ modules/snowflake_format_parquet/main.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.92.0"
+    }
+  }
+}
 
+resource "snowflake_file_format" "file_format" {
+  name        = "FORMAT_PARQUET"
+  database    = "SILVER_DEV"
+  schema      = "WELLSVIEW"
+  format_type = "PARQUET"
+}
