05a820d8c1357de7ae032a3ed76598f0776d5732
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 16:52:06 +0530

    Update main.tf

--- modules/snowflake_integration/main.tf
+++ modules/snowflake_integration/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.92.0"
+    }
+  }
+}
 
+resource "snowflake_storage_integration" "integration" {
+  name    = "INTEGRATION_SILVER_DEV"
+  comment = "A storage integration"
+  type    = "EXTERNAL_STAGE"
+  enabled = true
+  azure_tenant_id = '1aa51068-11a6-4bd2-8646-1fff31a30ffc'
+  storage_allowed_locations =('azure://edpdevarmdlsuw2001.blob.core.windows.net/bakeoff-snowflake/')
+}
