2572a5b9b19740e9ce7443f98c8b98b8500525bb
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 20:49:23 +0530

    Update variables.tf

--- Project1QUT/variables.tf
+++ Project1QUT/variables.tf
@@ -1,1 +1,20 @@
+variable "snowflake_username" {
+  description = "Username for the Snowflake account"
+  type        = string
+}
 
+variable "snowflake_account" {
+  description = "Account identifier for the Snowflake account"
+  type        = string
+}
+
+variable "snowflake_role" {
+  description = "Role to use for the Snowflake account"
+  type        = string
+}
+
+variable "snowflake_password" {
+  description = "Password for the Snowflake account"
+  type        = string
+  sensitive   = true
+}
