d4408958e35c15f726f6671396d4b5a431c297aa
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 18:48:05 +0530

    Update main.tf

--- DEV1/main.tf
+++ DEV1/main.tf
@@ -62,7 +62,7 @@ module "snowflake_schema_change_history" {
   is_managed = false
   is_transient = false
 }
-
+/*
 module "snowflake_file_format" {
   source           = "../modules/snowflake_format_parquet"
   name        = "FORMAT_PARQUET"
@@ -87,5 +87,5 @@ module "snowflake_stage" {
   url         = "azure://edpdevarmdlsuw2001.blob.core.windows.net/bakeoff-snowflake/Wellview_DL_type2"
   database    = "SILVER_DEV"
   schema      = "WELLSVIEW"
-}
+} */
 
