a200d89f3dcc0aaf4f52d648af924848a5734eab
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 16:38:12 +0530

    Update variables.tf

--- modules/snowflake_format_parquet/variables.tf
+++ modules/snowflake_format_parquet/variables.tf
@@ -1,1 +1,22 @@
+variable "name" {
+  description = "The name of the Snowflake file format to create."
+  type        = string
+}
 
+variable "database" {
+  description = "Under which snowflake database file format to be created"
+  type        = string
+  default     = ""
+}
+
+variable "schema" {
+  description = "Under which snowflake database schema file format to be created"
+  type        = string
+  default     = ""
+}
+
+variable "format_type " {
+  description = "what would be the type for file"
+  type        = string
+  default     = "PARQUET"
+}
