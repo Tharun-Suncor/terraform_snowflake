57a04b318cbfcaf3d054f06a3a9b67f390be0372
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 16:57:36 +0530

    Update variables.tf

--- modules/snowflake_integration/variables.tf
+++ modules/snowflake_integration/variables.tf
@@ -1,1 +1,27 @@
+variable "name" {
+  description = "The name of the Snowflake storage integration"
+  type        = string
+}
 
+variable "comment " {
+  description = "Any comment regarding integration"
+  type        = string
+}
+
+variable "type" {
+  description = "kind of storage"
+  type        = string
+}
+
+variable "enabled" {
+  description = "should enabled or not"
+  type        = boolean
+}
+variable "azure_tenant_id" {
+  description = "Mention azure tenant id"
+  type        = string
+}
+variable "storage_allowed_locations " {
+  description = "Mention location"
+  type        = string
+}
