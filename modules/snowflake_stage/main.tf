0145210da9bbf444f0be106ec2b047a663b71f18
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 17:05:56 +0530

    Update main.tf

--- modules/snowflake_stage/main.tf
+++ modules/snowflake_stage/main.tf
@@ -9,8 +9,7 @@ terraform {
 
 resource "snowflake_stage" "example_stage" {
   name        = "EXAMPLE_STAGE"
-  url         = "s3://com.example.bucket/prefix"
-  database    = "EXAMPLE_DB"
-  schema      = "EXAMPLE_SCHEMA"
-  credentials = "AWS_KEY_ID='${var.example_aws_key_id}' AWS_SECRET_KEY='${var.example_aws_secret_key}'"
+  url         = "azure://edpdevarmdlsuw2001.blob.core.windows.net/bakeoff-snowflake/Wellview_DL_type2"
+  database    = "SILVER_DEV"
+  schema      = "WELLSVIEW"
 }
