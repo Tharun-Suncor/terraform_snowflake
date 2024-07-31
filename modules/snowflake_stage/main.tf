a360785610df4a50994290e1d473e8ce03c9ca47
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 17:08:17 +0530

    Update main.tf

--- modules/snowflake_stage/main.tf
+++ modules/snowflake_stage/main.tf
@@ -7,8 +7,8 @@ terraform {
   }
 }
 
-resource "snowflake_stage" "example_stage" {
-  name        = "EXAMPLE_STAGE"
+resource "snowflake_stage" "stage" {
+  name        = "STAGE_SILVER"
   url         = "azure://edpdevarmdlsuw2001.blob.core.windows.net/bakeoff-snowflake/Wellview_DL_type2"
   database    = "SILVER_DEV"
   schema      = "WELLSVIEW"
