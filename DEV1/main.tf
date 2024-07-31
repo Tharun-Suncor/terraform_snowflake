94b9f8d89d72f8710d7bfe6a0b117178bbb893bc
vaisakh-suncor1 <vaip@suncor.com>
2024-07-31 18:16:41 +0530

    Update main.tf

--- DEV1/main.tf
+++ DEV1/main.tf
@@ -77,8 +77,8 @@ module "snowflake_storage_integration" {
   comment = "A storage integration"
   type    = "EXTERNAL_STAGE"
   enabled = true
-  azure_tenant_id = '1aa51068-11a6-4bd2-8646-1fff31a30ffc'
-  storage_allowed_locations =('azure://edpdevarmdlsuw2001.blob.core.windows.net/bakeoff-snowflake/')
+  azure_tenant_id = "1aa51068-11a6-4bd2-8646-1fff31a30ffc"
+  storage_allowed_locations =("azure://edpdevarmdlsuw2001.blob.core.windows.net/bakeoff-snowflake/")
 }
 
 module "snowflake_stage" {
