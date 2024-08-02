64d48f3012d76f75972cf907e3ca8f84fce5ad61
Tharun-Suncor <151141226+Tharun-Suncor@users.noreply.github.com>
2024-08-02 11:10:56 +0530

    Update main.tf

--- EDEnvDEV/main.tf
+++ EDEnvDEV/main.tf
@@ -9,24 +9,24 @@ terraform {
       version = "~> 3.0"
     }
   }
-}
-backend "azurerm" {
+
+  backend "azurerm" {
     resource_group_name  = "rg_sf_terraform_backend"
     storage_account_name = "saterraformbackend22"
     container_name       = "terrafornstate"
     key                  = "terraform.tfstate"
   }
-
+}
 
 provider "azurerm" {
   features {}
 }
 
 provider "snowflake" {
-  account     = var.snowflake_account
-  user        = var.snowflake_username
-  password    = var.snowflake_password
-  role        = var.snowflake_role
+  account  = var.snowflake_account
+  user     = var.snowflake_username
+  password = var.snowflake_password
+  role     = var.snowflake_role
 }
 
 module "snowflake_warehouse" {
