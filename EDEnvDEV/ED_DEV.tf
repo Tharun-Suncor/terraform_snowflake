e1346f55860e6b3ad3b68bea3b91d8480896f1dc
Kula Tharun Reddy Konkala <kkonkala@suncor.com>
2024-08-07 00:34:43 +0530

    update

---
+++ EDEnvDEV/ED_DEV.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.92.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  account  = var.snowflake_account
+  user     = var.snowflake_username
+  password = var.snowflake_password
+  role     = var.snowflake_role
+}
+
+# Snowflake Warehouses
+module "compute_wh_qut" {
+  source   = "../modules/snowflake_warehouse"
+  name     = "COMPUTE_WH_ED_QUT"
+  size     = "X-SMALL"
+  comment  = "Compute warehouse for QUT environment"
+  auto_suspend                          = 60
+  auto_resume                           = true
+  initially_suspended                   = true
+}
