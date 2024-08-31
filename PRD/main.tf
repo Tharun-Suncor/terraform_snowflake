3d725c0ac33b017ec1a8804d3de98e1699679a8c
Tharun-Suncor <151141226+Tharun-Suncor@users.noreply.github.com>
2024-08-31 14:40:57 +0530

    new

--- PRD/main.tf
+++ PRD/main.tf
@@ -16,33 +16,10 @@ provider "snowflake" {
 
 
 
-module "snowflake_database" {
-  source = "../modules/snowflake_database"
-
-  name                        = "SILVER_PRD1"
-  is_transient                = false
-  comment                     = "This is database"
-  data_retention_time_in_days = 1
-}
-
-
-module "snowflake_schema" {
-  source     = "../modules/snowflake_schema"
-  database   = "SILVER_PRD1"
-  schema     = "WELLSVIEW"
-  comment   = "This is a schema"
-  data_retention_days = -1
-  is_managed = false
-  is_transient = false
-depends_on = [module.snowflake_database]
-}
-
-
-
 module "snowflake_stage" {
   source           = "../modules/snowflake_stage"
   name        = "STAGE_SILVER"
-  database    = "SILVER_PRD1"
+  database    = "SILVER_PRD"
   schema      = "WELLSVIEW"
   comment     = "internal stage"
   tags = {
